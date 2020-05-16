882e73c61e49871936dea9695aa7520fe8dbc7ac
Marta <marta.colombas@travelperk.com>
2020-05-16 17:15:13 +0200

    feat: finished django get started

--- polls/urls.py
+++ polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:pk/', views.IndexView.as_view(), name='detail'),
+    path('<int:pk>/', views.IndexView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:pk/results/', views.IndexView.as_view(), name='results'),
+    path('<int:pk>/results/', views.IndexView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:pk/vote/', views.vote, name='vote'),
+    path('<int:pk>/vote/', views.vote, name='vote'),
 ]
